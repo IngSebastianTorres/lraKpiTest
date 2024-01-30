4460598e8b0123a76b6ff58a41c7fce3077e5f21
root <ingsebastiantorres95@gmail.com>
2024-01-30 12:24:50 -0500

    Ajuste en componentes e inclusion de notification push en  PROGRESIVE WEB APP con service worker

---
+++ src/app/services/push-notification-service.service.ts
@@ -0,0 +1,37 @@
+import { Injectable } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { firstValueFrom } from 'rxjs';
+
+/* 
+ Servicio que se encarga de enviar el token de autenticación para las notificaciones PUSH contra el backend donde esta alojado
+*/
+@Injectable({
+  providedIn: 'root'
+})
+export class PushNotificationServiceService {
+
+  public url ='http://localhost:9000';
+
+  constructor( private http:HttpClient) { 
+
+  }
+
+
+  async saveToken(token):Promise<any>{
+    let body = { token }
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': 'application/json; charset=UTF-8'
+      })
+    }
+    return await firstValueFrom(this.http.post<any>(this.url+"/save", body, httpOptions ));
+  }
+
+/*  saveToken = (token) => {
+    return this.http.post( this.url+"/save", 
+      body:{
+        token
+      }
+    );
+  };*/
+}
