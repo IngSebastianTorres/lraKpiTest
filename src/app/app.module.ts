82d3dc2cc538cdedd52911d5ded36ec138cf5b2b
root <ingsebastiantorres95@gmail.com>
2024-02-05 09:48:05 -0500

    avances en aplicacion LRA KPI Para manejo de login con firebase

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { AppComponent } from './app.component';
 import { AdminLayoutComponent } from './layouts/admin-layout/admin-layout.component';
 import { ServiceWorkerModule } from '@angular/service-worker';
 import { environment } from '../environments/environment';
+import { LoginComponent } from './login/login.component';
 
 @NgModule({
   imports: [
@@ -34,7 +35,8 @@ import { environment } from '../environments/environment';
   ],
   declarations: [
     AppComponent,
-    AdminLayoutComponent
+    AdminLayoutComponent,
+    LoginComponent
   ],
   providers: [],
   bootstrap: [AppComponent]
