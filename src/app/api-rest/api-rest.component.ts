608e1907c846ecb943e9222fad0d677e2fce0fd8
root <ingsebastiantorres95@gmail.com>
2024-02-12 08:56:08 -0500

    Cambios e integración de componente API - REST para la documentación de los endpoint en el apartado web de la página

--- src/app/api-rest/api-rest.component.ts
+++ src/app/api-rest/api-rest.component.ts
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ApiRestComponent implements OnInit {
 
-  constructor() { }
+  renderModalBackdrop:boolean;
 
-  ngOnInit(): void {
+  constructor() { 
+    this.renderModalBackdrop=false;
   }
 
+  ngOnInit(): void {
+    
+  }
+
+  /*
+    Se generan metodos para controlar los comportamientos de apertura y cierre de modal en el componente
+  */
+  onClickShowModal(id:string){
+    
+    document.getElementById(id).className= "modal fade in";
+    document.getElementById(id).style.display='block';
+    document.getElementById(id).style.overflowY= 'auto';
+    document.getElementById('app-main-body').style.overflowY='hidden';
+    document.getElementById('main-panel-application').className='openModalNoScrolling';
+
+    this.renderModalBackdrop=true;
+  }
+
+  closeModal(id:string){
+    document.getElementById(id).className= "modal fade";
+    document.getElementById(id).style.display='none';
+    document.getElementById('app-main-body').style.overflowY='auto';
+    document.getElementById('main-panel-application').className='main-panel ps ps--active-y';
+
+    this.renderModalBackdrop=false;
+  }
+  
 }
